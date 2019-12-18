f58ab5edb62a74cee1ecc0ab02483afc45f46303
Daniel Gill <dwgill@outlook.com>
2019-12-18 07:50:30 -0500

    Add CSS Support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import styles from './index.css';
+
 document.getElementById('root').appendChild((() => {
     const h1 = document.createElement('h1');
     h1.innerHTML = "Hello World!";
+    h1.classList.add(styles.header);
     return h1;
 })())
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = (env, argv) => {
     const isDev = argv.mode === 'development';
-    const isProd = argv.mode === 'development';
+    const isProd = argv.mode === 'production';
     const rootDir = path.resolve(__dirname);
 
+    let cssLoaders;
     return {
         entry: './src/index.js',
         output: {
@@ -18,14 +21,41 @@ module.exports = (env, argv) => {
                 {
                     test: /\.hbs$/,
                     loader: ['handlebars-loader'],
+                },
+                {
+                    test: /\.css/,
+                    loader: (cssLoaders = [
+                        isDev
+                            ? 'style-loader'
+                            : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    mode: 'local',
+                                    localIdentName: isDev
+                                        ? '[path][name]__[local]'
+                                        : '[hash:base64]',
+                                }
+                            }
+                        },
+                    ])
+                },
+                {
+                    test: /\.scss/,
+                    loader: cssLoaders.concat('sass-loader'),
                 }
             ]
         },
         plugins: [
+            new CleanWebpackPlugin(),
             new HtmlWebpackPlugin({
                 template: path.resolve(rootDir, 'src/index.hbs'),
                 title: "Hello World!"
-            })
+            }),
+            new MiniCssExtractPlugin({
+                filename: isDev ? 'bundle.css' : 'bundle.[contenthash].css',
+            }),
         ]
     };
 }
