eaba3ba663a2c0e8f0b97603f0b2c11a5f70f6b5
Daniel Gill <dwgill@outlook.com>
2019-12-27 08:23:15 -0500

    Adjustmnts back to a basic setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./App.scss";
+
+const App: React.FC<{}> = () => {
+  return <h1 className={styles.header}>Hello World!</h1>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from "./components/App/App";
+import { render } from "react-dom";
+import React from "react";
+
+render(<App />, document.getElementById("root"));
